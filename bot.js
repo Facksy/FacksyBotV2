3ae2b3468297e126ec92c2777cb46620371046f4
Facksy <45769049+Facksy@users.noreply.github.com>
2019-08-26 16:33:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,6 @@ client.on('message', message => {
   	}
 });
 
-BOT_TOKEN = NjE1NTQyMDYwNTEwMDg1MTM0.XWPlIw.AZz13o8hYi3dsZC-Jx2P2XRIEZs
+BOT_TOKEN = NjE1NTQyMDYwNTEwMDg1MTM0.XWPtxQ.ULuc0Qt-IWe1g219q_E9Zo1Jr9E
 
 client.login(process.env.BOT_TOKEN);
